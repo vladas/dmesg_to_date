e0d502b6c872dd0cb1b646642a247f699a021a6f
Vladas Diržys <vladas.dirzys@gmail.com>
2012-08-24 20:34:45 +0200

    CHANGE: made it working on mac os x

--- dmesg_to_date.py
+++ dmesg_to_date.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+# coding=utf8
 import os
 import re
+import string
 import subprocess
 
 def uptime_in_seconds():
@@ -11,35 +13,35 @@ def uptime_in_seconds():
           uptime = int(re.match('^\d+', line).group(0))
       return uptime
     else:
-      info = re.split('^\s(\d{2}.\d{2}.\d{2})\sup\s(\d+)\s(\w+),\s+(\d+)(:\d+|\w+)', subprocess.check_output('uptime'))
+      return uptime_cmd_to_seconds(subprocess.check_output('uptime'))
 
-      now       = info[1]
-      value     = int(info[2])
-      unit      = info[3]
-      precision = info[4]
-      surprise  = info[5]
+def uptime_cmd_to_seconds(uptime):
 
-      seconds = 0
+  seconds = 0
+  info = re.split('^.+up\s(.+),\s+\d+\susers', uptime)[1].split(',') # split into chunks
 
+  for time in info:
+    time = time.strip()
+    if re.search('(\d+)\:(\d+)', time): # match time, eg. 21:41
+      m = re.search('(\d+)\:(\d+)', time)
+      seconds += int(m.group(1)) * 60 * 60 # hours
+      seconds += int(m.group(2)) * 60 # mins
+    elif re.search('(\d+)\s+(\w+)', time): # match over units, eg. 2 days, 5 min
+      m = re.search('(\d+)\s+(\w+)', time)
+      value     = int(m.group(1))
+      unit      = m.group(2)
+      
       if unit == "days":
-        seconds = value*24*60*60
+        seconds += value*24*60*60
       elif unit == "hour":
-        seconds = value*60*60
+        seconds += value*60*60
       elif unit == "min":
-        seconds = value*60
-
-      # There has to be a better way to do this
-      m = re.match('^:(\d+)$', surprise)
-      if m:
-        hours   = int(precision)
-        minutes = int(m.group(1))
-        seconds += hours*60*60 + minutes*60
-      else:
-        m = re.match('min', surprise)
-        if m:
-          minutes = int(precision)
-          seconds += minutes*60
-
-      return seconds
-
+        seconds += value*60
+      
+  return seconds
+  
+''' OS X '''
+#print uptime_cmd_to_seconds('19:16  up 2 days, 21:41, 3 users, load averages: 3.01 2.60 1.93')
+''' Ubuntu '''
+#print uptime_cmd_to_seconds('﻿ 19:11:47 up 5 min,  2 users,  load average: 2.29, 2.14, 1.04')
 print 'System uptime in seconds: %d' % uptime_in_seconds()
