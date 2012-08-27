0263a16d96355dfb31c8423a6d6c8930d5adfbc9
Vladas Diržys <v.dirzys@sportradar.com>
2012-08-27 13:04:02 +0200

    Fixed syntax error, converred tabs to spaces

--- dmesg_to_date.php
+++ dmesg_to_date.php
@@ -2,43 +2,42 @@
 <?php
 function uptime_in_seconds()
 {
-	if (is_readable("/proc/uptime")) {
-		// parse ﻿778.53 from ﻿778.53 500.11
-		﻿$uptimeArr = explode(' ', file_get_contents('/proc/uptime'));
-	    return (int)$uptimeArr[0];
-	} else {
-		return uptime_cmd_to_seconds(`uptime`);
-	}
+    if (is_readable("/proc/uptime")) {
+        // parse ﻿778.53 from ﻿778.53 500.11
+        $uptimeArr = explode(' ', file_get_contents('/proc/uptime'));
+        return (int) $uptimeArr[0];
+    } else {
+        return uptime_cmd_to_seconds(`uptime`);
+    }
 }
 
 function uptime_cmd_to_seconds($uptime)
 {
-	$seconds = 0;
-	$matches = array();
-	if (preg_match('/^.+up\s(.+),\s+\d+\susers/', $uptime, $matches)) {
-		$timeArr = explode(',', $matches[1]);
-		foreach ($timeArr as $timeStr) {
-			$timeStr = trim($timeStr);
-			if (preg_match('/(\d+)\:(\d+)/', $timeStr, $matches)) {
-				$seconds += (int)$matches[1] * 60 * 60; # hours
-				$seconds += (int)$matches[2] * 60; # mins
-			} elseif (preg_match('/(\d+)\s+(\w+)/', $timeStr, $matches)) {
-				$value	= (int)$matches[1];
-				$unit	= (int)$matches[2];
-				if ($unit == "days") {
-					$seconds += $value*24*60*60;
-				} elseif ($unit == "hour") {
-					$seconds += $value*60*60;
-				} elseif ($unit == "min") {
-					$seconds += $value*60;
-				}
-			}
-		}
-	}
-	return $seconds;
+    $seconds = 0;
+    $matches = array();
+    if (preg_match('/^.+up\s(.+),\s+\d+\susers/', $uptime, $matches)) {
+        $timeArr = explode(',', $matches[1]);
+        foreach ($timeArr as $timeStr) {
+            $timeStr = trim($timeStr);
+            if (preg_match('/(\d+)\:(\d+)/', $timeStr, $matches)) {
+                $seconds += (int) $matches[1] * 60 * 60; # hours
+                $seconds += (int) $matches[2] * 60; # mins
+            } elseif (preg_match('/(\d+)\s+(\w+)/', $timeStr, $matches)) {
+                $value = (int) $matches[1];
+                $unit = (int) $matches[2];
+                if ($unit == "days") {
+                    $seconds += $value * 24 * 60 * 60;
+                } elseif ($unit == "hour") {
+                    $seconds += $value * 60 * 60;
+                } elseif ($unit == "min") {
+                    $seconds += $value * 60;
+                }
+            }
+        }
+    }
+    return $seconds;
 }
-
-echo 'System uptime in seconds: ', (int)uptime_in_seconds(), "\n";
+echo 'System uptime in seconds: ', (int) uptime_in_seconds(), "\n";
 
 // OS X
 //echo uptime_cmd_to_seconds('19:16  up 2 days, 21:41, 3 users, load averages: 3.01 2.60 1.93'), "\n";
